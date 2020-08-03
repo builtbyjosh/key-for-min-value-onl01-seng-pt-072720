27055dc7491466e7ea17552df1ef79f2bad0e83e
Joshua Collins <pc.refuge@gmail.com>
2020-08-03 18:45:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_name = ""
   lowest_value = ""
-  name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
-      lowest_value = value
-      lowest_name = key
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_name = k
     end
   end
   lowest_name
