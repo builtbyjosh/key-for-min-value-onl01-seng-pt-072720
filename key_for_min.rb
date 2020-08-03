e515635439b51939f2bf29be5ca0c129cc84cb8d
Joshua Collins <pc.refuge@gmail.com>
2020-08-03 18:45:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_name = ""
+  lowest_value = ""
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_name = key
+    end
+  end
+  lowest_name
 end
